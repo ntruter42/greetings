188215ec127cadb02a43a99bbfdbf9b9576d0b9d
Nicholas Truter <ntruter42@gmail.com>
2023-05-05 15:18:43 +0200

    Add e7jj6r.jpg, Update greet.css, greet-dom.js - added background image, improved styling, basic validation for input

--- js/greet-dom.js
+++ js/greet-dom.js
@@ -4,9 +4,23 @@ const message = document.querySelector('#greet-message');
 
 const greet = Greet();
 
-button.addEventListener('click', function () {
-	greet.setName(input.value);
-	greet.setMessage();
+function greetButtonClick() {
+	if (input.value === "") {
+		alert("Name can't be empty");
+	} else {
+		greet.setName(input.value);
+		greet.setMessage();
 
-	message.innerHTML = greet.getMessage();
+		message.innerHTML = greet.getMessage();
+		input.value = "";
+	}
+}
+
+input.addEventListener("keydown", function (event) {
+	if (event.keyCode === 13) {
+		event.preventDefault();
+		greetButtonClick();
+	}
 });
+
+button.addEventListener('click', greetButtonClick);
