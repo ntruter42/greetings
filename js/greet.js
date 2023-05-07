90bf7885e697899fc75579e633328aaecbcd4e43
ntruter42 <ntruter42@gmail.com>
2023-05-07 18:18:43 +0200

    Update index.html, greet.css, greet.js - added html skeleton code for radio buttons and counter value display

--- js/greet.js
+++ js/greet.js
@@ -1,6 +1,7 @@
 function Greet() {
 	let name = "";
 	let message = "";
+	let namesGreeted = {};
 
 	function setName(input) {
 		name = input.trim();
@@ -15,13 +16,37 @@ function Greet() {
 	}
 
 	function getMessage() {
+		addName();
+		countGreeting();
 		return message;
+	}
+
+	function getCount() {
+		return namesGreeted.length;
+	}
+
+	function addName() {
+		if (!hasBeenGreeted) {
+			namesGreeted[name] = 1;
+		} else {
+
+		}
+	}
+
+	function hasBeenGreeted() {
+		if (namesGreeted[name] === undefined) {
+			return false;
+		}
+		return true;
 	}
 
 	return {
 		setName,
 		getName,
 		setMessage,
-		getMessage
+		getMessage,
+		getCount,
+		addName,
+		hasBeenGreeted
 	}
 }
